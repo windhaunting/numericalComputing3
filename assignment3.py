bb032752793ce23e444f30ddcd1179b42fa0158d
fubao <freadream@gmail.com>
2017-09-25 23:41:58 -0400

    update

--- assignment3.py
+++ assignment3.py
@@ -6,7 +6,14 @@ import numpy as np
 housing_data = np.loadtxt("boston.csv",delimiter=',')
         
 ## Add code here ##
-        
+#question 1:
+#get total entry 
+totalNum = housing_data.size
+totalNan = np.sum(np.isnan(housing_data)) #np.count_nonzero(np.isnan(housing_data))
+print ("percentage: ", totalNan, totalNum, totalNan/totalNum)
+
+
+
 ####################################################
 # Problem 2: Data exploration
 ####################################################
